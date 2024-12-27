7d7449883ee36ca62d6b5615b7198421ee2d654c
fazza-tech <ebranfazza@gmail.com>
2024-12-27 20:42:01 +0530

    App updated

---
+++ react_form/src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+/**
+ * Challenge: add another label and input for the password field
+ */
+
+export default function App() {
+  return (
+    <section>
+      <h1>Signup form</h1>
+      <form>
+        <label htmlFor="email">Email:</label>
+        <input id="email" type="email" name="email" placeholder="joe@schmoe.com" />
+        <br />
+        
+        
+        
+      </form>
+    </section>
+  )
+}
+
