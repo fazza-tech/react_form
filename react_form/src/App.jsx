671113b7e635bf3fca283b1f834290bfd63e990e
fazza-tech <ebranfazza@gmail.com>
2024-12-28 04:51:11 +0530

    updated jsx

--- react_form/src/App.jsx
+++ react_form/src/App.jsx
@@ -1,23 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
-/**
- * Challenge: add another label and input for the password field
- */
+import './index.css'; // Importing the CSS file
 
 export default function App() {
+
+  function handleSubmit(event){
+    event.preventDefault()
+    console.log("submitted")
+  }
   return (
-    <section>
-      <h1>Signup form</h1>
-      <form>
+    <section className="form-container">
+      <h1 className="form-title">Signup Form</h1>
+      <form className="signup-form" onSubmit={handleSubmit}>
         <label htmlFor="email">Email:</label>
         <input id="email" type="email" name="email" placeholder="joe@schmoe.com" />
-        <br />
-        
-        
-        
+        <label htmlFor="password">Password:</label>
+        <input id="password" type="password" name="password" placeholder="Enter your password" />
+        <button type="submit">Submit</button>
       </form>
     </section>
-  )
+  );
 }
-
