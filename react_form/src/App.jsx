e85020bafadbef419fd2b03d62c728166f850539
fazza-tech <ebranfazza@gmail.com>
2024-12-30 07:34:01 +0530

    form updated

--- react_form/src/App.jsx
+++ react_form/src/App.jsx
@@ -4,14 +4,26 @@ import './index.css'; // Importing the CSS file
 export default function App() {
 
   function signUp(formData){
-    const email = formData.get("email")
-    const password = formData.get("password")
-    const employmentStatus = formData.get("employment-status")
-    const description = formData.get("description")
-    console.log("email :"+email)
-    console.log("password : "+password)
-    console.log(description)
-    console.log(employmentStatus)
+    // const email = formData.get("email")
+    // const password = formData.get("password")
+    // const employmentStatus = formData.get("employment-status")
+    // const description = formData.get("description")
+    // const dieteryRestriction = formData.getAll("Dietery-status")
+    // const favCOlor = formData.getAll("favColor")
+    // console.log("email :"+email)
+    // console.log("password : "+password)
+    // console.log(description)
+    // console.log(employmentStatus)
+    // console.log(dieteryRestriction)
+    // console.log("color:"+favCOlor)
+
+    const data = Object.fromEntries(formData);
+    const dieteryData = formData.getAll("Dietery-status")
+    const allData = {
+      ...data,
+      dieteryData
+    }
+    console.log(allData)
   }
   return (
     <section className="form-container">
@@ -37,11 +49,41 @@ export default function App() {
           Part-time
         </label>
         <label className='radio-label'>
-          <input type="radio" name='employment-status' value=" Full-time"/>
+          <input type="radio" name='employment-status' value=" Full-time" defaultChecked={true}/>
           Full-time
         </label>
+
+      </fieldset>
+
+      <fieldset>
+        <legend>Dietery Restriction :</legend>
+        <label className='radio-label'>
+          <input type="checkbox" name='Dietery-status' value="Kosher" />
+           Kosher
+        </label>
+        <label className='radio-label'>
+          <input type="checkbox" name='Dietery-status' value="Vegan" />
+          Vegan
+        </label>
+        <label className='radio-label'>
+          <input type="checkbox" name='Dietery-status' value=" Glutten-free" defaultChecked={true}/>
+          Glutten-free
+        </label>
+
       </fieldset>
         
+        <label htmlFor="favColor">
+          <select name="favColor" id="favColor" defaultValue="" required >
+            <option value="" disabled >--Choose an Option--</option>
+            <option value="Green">Black</option>
+            <option value="Yelleow">Yelleow</option>
+            <option value="Orange">Orange</option>
+            <option value="Indigo">Indigo</option>
+            <option value="Blue">Blue</option>
+            <option value="Red">Red</option>
+            <option value="White">White</option>
+          </select>
+        </label>
 
         <button type="submit">Submit</button>
       </form>
